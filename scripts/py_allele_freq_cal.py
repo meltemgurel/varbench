fdbfb8856908d7b2ddc802f2bd9e19156048e00f
Meltem Gurel <gurel01@clust1-headnode-1.cri.camres.org>
2017-01-03 13:48:54 +0000

    np

--- scripts/py_allele_freq_cal.py
+++ scripts/py_allele_freq_cal.py
@@ -2,25 +2,42 @@ import sys
 import pysam
 import pandas
 
-def get_af(bam, chr, pos):
+def get_allele_frequencies(bam, chr, pos):
     pos -= 1
-    baseFreq = {"A":0,"T":0,"C":0,"G":0}
-    bamFile = pysam.AlignmentFile(bam, 'rb')
-    for pileupcolumn in bamFile.pileup(chr, pos):
+    base_freq = {"A":0,"T":0,"C":0,"G":0}
+    bam_file = pysam.AlignmentFile(bam, 'rb')
+    for pileupcolumn in bam_file.pileup(chr, pos, max_depth = 10000):
         found = []
         if pileupcolumn.pos == pos:
+            #print pileupcolumn.n
             for pileupread in pileupcolumn.pileups:
-                if not pileupread.is_del and not pileupread.is_refskip:
+                if pileupread.query_position is not None \
+                        and not pileupread.alignment.is_secondary \
+                        and bin(pileupread.alignment.flag & 2048) != bin(2048) \
+                        and not pileupread.alignment.mate_is_unmapped:
                     try:
                         found.index(pileupread.alignment.qname)
                     except ValueError :
-                        baseFreq[str(pileupread.alignment.seq[pileupread.query_position])] += 1
+                        base_freq[str(pileupread.alignment.seq[pileupread.query_position])] += 1
                         found.append(pileupread.alignment.qname)
-    bamFile.close()
-    return baseFreq
+    bam_file.close()
 
-df = pandas.DataFrame.from_csv(sys.argv[1], sep='\t', index_col=None, header=None)
-for index, row in df.iterrows():
-    print(get_af(bam=sys.argv[2], chr=str(row[0]), pos=int(row[1])))
-    print(get_af(bam=sys.argv[3], chr=str(row[0]), pos=int(row[1])))
-    print("-----------------------------------------------------------")
+    total = sum(base_freq.itervalues(), 0.0)
+    freqs = {k: round(v / total, 3) for k, v in base_freq.iteritems()}
+    print freqs
+    return total, freqs
+
+in_df  = pandas.DataFrame.from_csv(sys.argv[1], sep='\t',
+                                   index_col=None, header=None)
+out_df = pandas.DataFrame(index=range(0,len(in_df.index)),
+                          columns=['CHR','POS','COV','REF','ALT','VAF','RVAF'])
+
+for index, row in in_df.iterrows():
+    total, freqs = get_allele_frequencies(bam = sys.argv[2],
+                                          chr = row[0],
+                                          pos = row[1])
+    out_df.loc[index] = [row[0], row[1], total,
+                         row[2], row[3], row[4],
+                         freqs[row[3]]]
+
+print out_df
